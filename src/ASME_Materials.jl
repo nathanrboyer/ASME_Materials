a6c772a2f0b7e8949ee46c1da2d968b369aa57ff
nathanrboyer <nathanrobertboyer@gmail.com>
2022-05-05 17:24:21 -0400

    test4

--- src/ASME_Materials.jl
+++ src/ASME_Materials.jl
@@ -24,6 +24,10 @@ function process()
     include(joinpath(@__DIR__, "PlotTables.jl"))
 end
 
-export process
+for n in names(@__MODULE__; all=true)
+    if Base.isidentifier(n) && n ∉ (Symbol(@__MODULE__), :eval, :include)
+        @eval export $n
+    end
+end
 
 end # module
