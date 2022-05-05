d064adf870a2ca19cf863c7fa5656b58b298dee9
nathanrboyer <nathanrobertboyer@gmail.com>
2022-05-05 16:41:49 -0400

    test

--- src/ASME_Materials.jl
+++ src/ASME_Materials.jl
@@ -5,7 +5,7 @@ const specno = "SA-723"
 const type_grade = 3
 const class_condition_temper = 2
 const AIP_material_category = "AIP Q&T Steels"
-const tableKM620_material_category = "Ferritic steel"
+const tableKM620_material_category = "Ferritic steel" # NOTE: Ferritic steel includes carbon, low alloy, and alloy steels, and ferritic, martensitic, and iron-based age-hardening stainless steels.
 const num_output_stress_points = 50 # Number of points to compute on each stress-strain curve
 
 # File Paths
@@ -16,10 +16,14 @@ const outputdir = joinpath("S:/Material Properties/Excel Material Data", AIP_mat
 using DataFrames, Interpolations, Latexify, PrettyTables, XLSX
 
 # Run Files
-include("ReadTables.jl")
-include("KM620.jl")
-include("BuildTables.jl")
-include("WriteTables.jl")
-include("PlotTables.jl")
+function run()
+    include(joinpath(@__DIR__, "ReadTables.jl"))
+    include(joinpath(@__DIR__, "KM620.jl"))
+    include(joinpath(@__DIR__, "BuildTables.jl"))
+    include(joinpath(@__DIR__, "WriteTables.jl"))
+    include(joinpath(@__DIR__, "PlotTables.jl"))
+end
+
+export run
 
 end # module
