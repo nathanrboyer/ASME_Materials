0aa0b117212c9ebdf27166f68be08499baca11c8
nathanrboyer <nathanrobertboyer@gmail.com>
2022-05-12 13:15:07 -0400

    Added progress messages

--- src/ASME_Materials.jl
+++ src/ASME_Materials.jl
@@ -3,7 +3,7 @@ module ASME_Materials
 # Welcome Message
 @info("""You have just loaded the ASME_Materials package!
 \t Ensure the material you need has been added to every sheet of the file `Section II-D Tables.xlsx`.
-\t Then type `main()` and press Enter.
+\t Then, once you see the `julia>` prompt, type `main()` and press Enter.
 """)
 
 # Load Packages
@@ -19,12 +19,18 @@ include("PlotTables.jl")
 
 # Collect Functions Together
 function main()
+    println("Processing KM620")
     user_input = get_user_input()
+    println("Reading Input File")
     ASME_tables, ASME_groups = read_ASME_tables(user_input.inputfilepath)
+    println("Transforming Tables")
     ANSYS_tables = transform_ASME_tables(ASME_tables, ASME_groups)
+    println("Writing Output Tables")
     write_ANSYS_tables(ANSYS_tables)
+    println("Plotting Results")
     fig1, fig2, fig3, fig4 = plot_ANSYS_tables(ANSYS_tables)
     display(fig4)
+    println("Complete")
     return ANSYS_tables
 end
 
